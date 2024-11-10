dd34b62cdef06bd36467ce453a805b47190ec9c2
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2024-11-10 20:15:59 +0530

    Delete soradb directory

--- soradb/__init__.py
+++
@@ -1,3 +0,0 @@
-from .soradb import soradb
-
-__all__ = ["connect", "drop_collection", "insert_one", "insert_many", "find_one", "update_one", "delete_one", "delete_many", "find", "sort_by", "count", "fetch_values_by_key", "version"]
