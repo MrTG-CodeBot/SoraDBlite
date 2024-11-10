01806a775f9cbc6a2f2a9e6ef85f30bc2bb24707
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2024-11-10 15:34:01 +0530

    Delete pymedia directory

--- pymedia/__init__.py
+++
@@ -1,4 +0,0 @@
-from .pinterest import download_pin
-
-__all__ = ["download_pin", "bard_ai", "show", "add", "multi", "division", "timedate", "subtract", "sqrt", "Random']
-
--- pymedia/cli.py
+++
@@ -1,2 +0,0 @@
-def main():
-    print("Hello\ncheck out the how to use the package: https://github.com/MrTG-CodeBot/pymedia/blob/main/README.md")
