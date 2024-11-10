cb32fe799d96915dff97e8161c888c1bad19fabd
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2024-11-10 15:38:17 +0530

    Update __init__.py

--- soradb/__init__.py
+++ soradb/__init__.py
@@ -1,3 +1,3 @@
-from .pinterest import download_pin
+from .soradb import *
 
-__all__ = []
+__all__ = ["connect", "drop_collection", "insert_one", "insert_many", "find_one", "update_one", "delete_one", "delete_many", "find", "sort_by"]
