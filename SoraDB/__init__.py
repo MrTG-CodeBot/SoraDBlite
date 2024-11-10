df84d6e132eb2832e78557b4498178cb61cd338b
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2024-11-10 20:15:30 +0530

    Create __init__.py

---
+++ SoraDB/__init__.py
@@ -0,0 +1,3 @@
+from .SoraDB import SoraDB
+
+__all__ = ["connect", "drop_collection", "insert_one", "insert_many", "find_one", "update_one", "delete_one", "delete_many", "find", "sort_by", "count", "fetch_values_by_key", "version"]
