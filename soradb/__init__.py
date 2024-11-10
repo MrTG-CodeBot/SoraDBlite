e7916f2a09b091bbdb61f6d38559d32ad6bf7e5c
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2024-11-10 19:50:04 +0530

    Update __init__.py

--- soradb/__init__.py
+++ soradb/__init__.py
@@ -1,3 +1,3 @@
 from .soradb import *
 
-__all__ = ["connect", "drop_collection", "insert_one", "insert_many", "find_one", "update_one", "delete_one", "delete_many", "find", "sort_by"]
+__all__ = ["connect", "drop_collection", "insert_one", "insert_many", "find_one", "update_one", "delete_one", "delete_many", "find", "sort_by", "count", "fetch_values_by_key", "version"]
