b4cffc34dde6ba5622645f579ecd19105a00f4ad
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2024-11-13 00:33:12 +0530

    Update __init__.py

--- SoraDBlite/__init__.py
+++ SoraDBlite/__init__.py
@@ -1,5 +1,3 @@
 from .SoraDBlite import SoraDBlite, SoraDBLiteError
 
-__version__ = "1.0.6"
-
-__all__ = ["connect", "drop_collection", "insert_one", "insert_many", "find_one", "update_one", "delete_one", "delete_many", "find", "sort_by", "count", "fetch_values_by_key", "version"]
+__all__ = ["connect", "drop_collection", "insert_one", "insert_many", "find_one", "update_one", "delete_one", "delete_many", "find", "sort_by", "count", "fetch_values_by_key", "version", "sora_ai"]
