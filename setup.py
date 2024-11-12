6ea181cbb588a806b8e7689d3d74ee3d736d664c
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2024-11-12 22:45:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name="SoraDBlite",
-    version="1.0.4",
+    version="1.0.6",
     description="SoraDBlite is a Python class designed to simplify interactions with MongoDB databases, the operation of SoraDBlite is similar to the pymongo. It is also a lite version of pymongo.Added Sora AI integration for error detection and solution.",
     long_description=pathlib.Path("README.md").read_text(encoding='utf-8'),
     long_description_content_type="text/markdown",
